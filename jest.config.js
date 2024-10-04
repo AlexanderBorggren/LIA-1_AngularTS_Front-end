a5d8101aabb52c54f4791866aeec03536d29f747
feeloor <felix@khoi.se>
2024-10-04 19:26:51 +0200

    adding jest instead of karma

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  coverageThreshold: {
+    global: {
+      statements: 75,
+      branches: 75,
+      functions: 75,
+      lines: 75,
+    },
+  },
+};
