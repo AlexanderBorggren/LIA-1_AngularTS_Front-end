8887db9aaf369d94398dc4994bb030564ea775d5
Alexander Borggren <borggren4@gmail.com>
2024-10-18 08:39:56 +0800

    restructure, added routes for userbar, added homecomponent as index route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { profileRoutes } from './profile/profile.routes';
+import { homeRoutes } from './home/home.routes';
+import { favoriteRoutes } from './favorite/favorite.routes';
+import { cartRoutes } from './cart/cart.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [...homeRoutes, ...profileRoutes, ...favoriteRoutes, ...cartRoutes];
