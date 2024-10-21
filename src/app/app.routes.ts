24b1bad130e2365c261957fe2f10ed2224e111b1
Alexander Borggren <borggren4@gmail.com>
2024-10-22 02:57:31 +0800

    added routes for all navbar and userbar, added product-list-view called from api with specific category, styling, moved landing to home component, added service and interface for products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,12 @@ import { profileRoutes } from './profile/profile.routes';
 import { homeRoutes } from './home/home.routes';
 import { favoriteRoutes } from './favorite/favorite.routes';
 import { cartRoutes } from './cart/cart.routes';
+import { productListRoutes } from './product-list/product-list.routes';
 
-export const routes: Routes = [...homeRoutes, ...profileRoutes, ...favoriteRoutes, ...cartRoutes];
+export const routes: Routes = [
+  ...homeRoutes,
+  ...profileRoutes,
+  ...favoriteRoutes,
+  ...cartRoutes,
+  ...productListRoutes,
+];
